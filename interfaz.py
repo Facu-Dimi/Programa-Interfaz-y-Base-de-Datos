fc5593f3866d129c2455551141c02e0ce94c6ea6
Facu-Dimi <147440653+Facu-Dimi@users.noreply.github.com>
2023-10-09 21:03:44 -0300

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -1,8 +1,6 @@
 from tkinter import Entry, Label, Frame, Tk, Button,ttk, Scrollbar, VERTICAL, HORIZONTAL,StringVar,END
 from conexion import *
 
-base_datos = Registro_datos()
-
 class Registro(Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
@@ -31,16 +29,16 @@ class Registro(Frame):
         Label(self.frame1, text = 'R E G I S T R O \t D E \t D A T O S',bg='gray22',fg='white', font=('Orbitron',15,'bold')).grid(column=0, row=0)
         
         Label(self.frame2, text = 'Agregar Nuevos Datos',fg='white', bg ='navy', font=('Rockwell',12,'bold')).grid(columnspan=2, column=0,row=0, pady=5)
-        Label(self.frame2, text = 'DNI',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=1, pady=15)
-        Label(self.frame2, text = 'Hectarea Designada',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=2, pady=15)
-        Label(self.frame2, text = 'Nombre Completo',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=3, pady=15)
-        Label(self.frame2, text = 'Direccion', fg='white',bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=4, pady=15)
-        Label(self.frame2, text = 'Salario',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=5, pady=15)
-        Label(self.frame2, text = 'Contacto',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=6, pady=15)
+        Label(self.frame2, text = 'dni',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=1, pady=15)
+        Label(self.frame2, text = 'hectareadesignada',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=2, pady=15)
+        Label(self.frame2, text = 'nombrecompleto',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=3, pady=15)
+        Label(self.frame2, text = 'direccion', fg='white',bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=4, pady=15)
+        Label(self.frame2, text = 'salario',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=5, pady=15)
+        Label(self.frame2, text = 'contacto',fg='white', bg ='navy', font=('Rockwell',13,'bold')).grid(column=0,row=6, pady=15)
 
 
         Entry(self.frame2,textvariable=self.dni , font=('Arial',12)).grid(column=1,row=1, padx =5)
-        Entry(self.frame2,textvariable=self.hectareadesignanda , font=('Arial',12)).grid(column=1,row=2)
+        Entry(self.frame2,textvariable=self.hectareadesignada , font=('Arial',12)).grid(column=1,row=2)
         Entry(self.frame2,textvariable=self.nombrecompleto , font=('Arial',12)).grid(column=1,row=3)
         Entry(self.frame2,textvariable=self.direccion , font=('Arial',12)).grid(column=1,row=4)
         Entry(self.frame2,textvariable=self.salario , font=('Arial',12)).grid(column=1,row=5)
@@ -48,7 +46,7 @@ class Registro(Frame):
        
         Label(self.frame4, text = 'Control',fg='white', bg ='black', font=('Rockwell',12,'bold')).grid(columnspan=3, column=0,row=0, pady=1, padx=4)         
         Button(self.frame4,command= self.agregar_datos, text='REGISTRAR', font=('Arial',10,'bold'), bg='magenta2').grid(column=0,row=1, pady=10, padx=4)
-        #Button(self.frame4,command = self.limpiar_datos, text='LIMPIAR', font=('Arial',10,'bold'), bg='orange red').grid(column=1,row=1, padx=10)        
+        Button(self.frame4,command = self.limpiar_datos, text='LIMPIAR', font=('Arial',10,'bold'), bg='orange red').grid(column=1,row=1, padx=10)        
         #Button(self.frame4,command = self.eliminar_fila, text='ELIMINAR', font=('Arial',10,'bold'), bg='yellow').grid(column=2,row=1, padx=4)
         #Button(self.frame4,command = self.buscar_nombre, text='BUSCAR POR NOMBRE', font=('Arial',8,'bold'), bg='orange').grid(columnspan=2,column = 1, row=2)
         #Entry(self.frame4,textvariable=self.buscar , font=('Arial',12), width=10).grid(column=0,row=2, pady=1, padx=8)
@@ -65,21 +63,23 @@ class Registro(Frame):
 
         self.tabla.configure(xscrollcommand = ladox.set, yscrollcommand = ladoy.set)
        
-        self.tabla['columns'] = ('dni', 'hectareadesignada', 'nombrecompleto', 'direccion', 'salario', 'contacto')
+        self.tabla['columns'] = ('hectareadesignada', 'nombrecompleto', 'direccion', 'salario', 'contacto')
 
-        self.tabla.column('#0', minwidth=100, width=120, anchor='center')
-        self.tabla.column('HectareaDesignada', minwidth=100, width=130 , anchor='center')
-        self.tabla.column('NombreCompleto', minwidth=100, width=120, anchor='center' )
-        self.tabla.column('Direccion', minwidth=100, width=120 , anchor='center')
-        self.tabla.column('Salario', minwidth=100, width=105, anchor='center')
-        self.tabla.column('Contacto', minwidth=100, width=105, anchor='center')
+        self.tabla.column('#0', minwidth=100, width=130, anchor='center')
+        self.tabla.column('hectareadesignada', minwidth=100, width=130 , anchor='center')
+        self.tabla.column('nombrecompleto', minwidth=100, width=120, anchor='center' )
+        self.tabla.column('direccion', minwidth=100, width=120 , anchor='center')
+        self.tabla.column('salario', minwidth=100, width=105, anchor='center')
+        self.tabla.column('contacto', minwidth=100, width=105, anchor='center')
 
-        self.tabla.heading('#0', text='DNI', anchor ='center')
-        self.tabla.heading('HectareaDesignada', text='HectareaDesignada', anchor ='center')
-        self.tabla.heading('NombreCompleto', text='NombreCompleto', anchor ='center')
-        self.tabla.heading('Direccion', text='Direccion', anchor ='center')
-        self.tabla.heading('Salario', text='Salario', anchor ='center')
-        self.tabla.heading('Contacto', text='Contacto', anchor ='center')
+
+        self.tabla.heading('#0', text='dni', anchor ='center')
+        self.tabla.heading('hectareadesignada', text='hectareadesignada', anchor ='center')
+        self.tabla.heading('nombrecompleto', text='nombrecompleto', anchor ='center')
+        self.tabla.heading('direccion', text='direccion', anchor ='center')
+        self.tabla.heading('salario', text='salario', anchor ='center')
+        self.tabla.heading('contacto', text='contacto', anchor ='center')
+
 
 
         estilo = ttk.Style(self.frame3)
@@ -93,12 +93,12 @@ class Registro(Frame):
 
     def agregar_datos(self):
         self.tabla.get_children()
-        dni = self.codigo.get() 
-        hectareadesignada = self.codigo.get() 
-        nombrecompleto = self.codigo.get() 
-        direccion = self.codigo.get()
-        salario = self.codigo.get() 
-        contacto = self.codigo.get()
+        dni = self.dni.get() 
+        hectareadesignada = self.hectareadesignada.get() 
+        nombrecompleto = self.nombrecompleto.get() 
+        direccion = self.direccion.get()
+        salario = self.salario.get() 
+        contacto = self.contacto.get()
         datos = (dni, hectareadesignada, nombrecompleto, direccion, salario, contacto)
         if dni and hectareadesignada and nombrecompleto and direccion and salario and contacto !='':        
             self.tabla.insert('',0, text = dni, values=datos)
@@ -132,7 +132,7 @@ class Registro(Frame):
         i = -1
         for dato in registro:
             i= i+1                       
-            self.tabla.insert('',i, text = registro[i][1:2], values=registro[i][2:6])
+            self.tabla.insert('',i, text = registro[i][0:1], values=registro[i][1:6])
     
     '''
     def eliminar_fila(self):
@@ -141,7 +141,7 @@ class Registro(Frame):
             self.tabla.delete(fila)
             nombre = ("'"+ str(self.nombre_borar) + "'")       
             self.base_datos.elimina_productos(nombre)
-            
+    '''      
     
     
     def obtener_fila(self, event):
@@ -150,20 +150,17 @@ class Registro(Frame):
             return
         data = self.tabla.item(current_item)
         self.nombre_borar = data['values'][0]
-    '''
    
 
 def main():
     ventana = Tk()
     ventana.wm_title("Registro de Datos en MySQL")
     ventana.config(bg='gray22')
-    ventana.geometry('900x500')
+    ventana.geometry('1300x510')
     ventana.resizable(0,0)
     app = Registro(ventana)
-    app.mainloop()
-
-if __name__=="__main__":
-    main()        
+    app.mainloop()  
+main()         
 
 
 
