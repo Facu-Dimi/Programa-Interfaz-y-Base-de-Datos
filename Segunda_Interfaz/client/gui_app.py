bee6df08fde6b287f269261012e9c61fec88a48c
Facu-Dimi <147440653+Facu-Dimi@users.noreply.github.com>
2023-10-14 00:28:37 -0300

    Update gui_app.py

--- Segunda_Interfaz/client/gui_app.py
+++ Segunda_Interfaz/client/gui_app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, END
+from client.conexion2 import *
+
 
 
 def barra_menu(root):
@@ -24,13 +26,43 @@ def barra_menu(root):
 
 
 
+class Login(tk.Frame):
+    
+    def __init__(self, root = None):
+        super().__init__(root, width = 480, height = 320)
+        self.root = root
+        self.pack()
+        #self.config(bg = 'blue')
+        self.clave = 'CuidaLasNetbooks'
+        self.clave_ingresada = tk.StringVar()
+        self.password()
+        
+    def password(self):
 
+        self.label_contrasena = tk.Label(self, text = 'Ingrese la contraseña:')
+        self.label_contrasena.config(font = ('Arial', 12, 'bold'))
+        self.label_contrasena.grid(row = 0, column = 0, padx = 8, pady = 8)
+        
+        self.entry_contrasena = tk.Entry(self, textvariable = self.clave_ingresada)
+        self.entry_contrasena.config(width = 50, font = ('Arial', 11), show = "*")
+        self.entry_contrasena.grid(row = 1, column = 0, padx = 8, pady = 8)
+        
+    
+        self.boton_ingresar = tk.Button(self, command = self.comprobar, text = 'Ingresar')
+        self.boton_ingresar.config(width = 20, font = ('Arial', 11, 'bold'), fg = 'snow', bg = 'SteelBlue2',
+                                cursor = 'hand2', activebackground = 'SkyBlue1')
+        self.boton_ingresar.grid(row = 2, column = 0, padx = 10, pady = 10)
+        
 
-
-
-def clicker(num):
-    num += 1
-    print(num) 
+    def comprobar(self):
+        clave_ingresada = self.clave_ingresada.get()
+        if clave_ingresada == self.clave:
+            self.destroy()
+            barra_menu(root = self.root)
+            Frame()
+        else:
+            self.entry_contrasena.delete(0, END)
+        print(clave_ingresada)
 
 
 class Frame(tk.Frame):
@@ -39,10 +71,18 @@ class Frame(tk.Frame):
         super().__init__(root, width = 480, height = 320)
         self.root = root
         self.pack()
-        #self.config(bg = 'blue')        
-        
         self.campos_pedido()
         self.tabla_datos()
+
+        #self.numero = StringVar()
+        #self.alumno = StringVar()
+        #self.curso = StringVar()
+        #self.profesor = StringVar()
+        #self.entrega = StringVar()
+        #self.devolucion = StringVar()
+
+        #self.base_datos = Registro_datos()
+        
     
     
     def campos_pedido(self):
@@ -77,35 +117,35 @@ class Frame(tk.Frame):
         # Entrys[Cuadros de texto] para escribir datos:
         
         self.entry_numero = tk.Entry(self)
-        self.entry_numero.config(width = 50, state = 'disabled', font = ('Arial', 11))
+        self.entry_numero.config(width = 50, font = ('Arial', 11))
         self.entry_numero.grid(row = 0, column = 1, padx = 8, pady = 8)
         
         self.entry_alumno = tk.Entry(self)
-        self.entry_alumno.config(width = 50, state = 'disabled', font = ('Arial', 11))
+        self.entry_alumno.config(width = 50, font = ('Arial', 11))
         self.entry_alumno.grid(row = 1, column = 1, padx = 8, pady = 8)
         
         self.entry_curso = tk.Entry(self)
-        self.entry_curso.config(width = 50, state = 'disabled', font = ('Arial', 11))
+        self.entry_curso.config(width = 50, font = ('Arial', 11))
         self.entry_curso.grid(row = 2, column = 1, padx = 8, pady = 8)
         
         self.entry_profesor = tk.Entry(self)
-        self.entry_profesor.config(width = 50, state = 'disabled', font = ('Arial', 11))
+        self.entry_profesor.config(width = 50, font = ('Arial', 11))
         self.entry_profesor.grid(row = 3, column = 1, padx = 8, pady = 8)
         
         self.entry_entrega = tk.Entry(self)
-        self.entry_entrega.config(width = 50, state = 'disabled', font = ('Arial', 11))
+        self.entry_entrega.config(width = 50, font = ('Arial', 11))
         self.entry_entrega.grid(row = 4, column = 1, padx = 8, pady = 8)
         
         self.entry_devolucion = tk.Entry(self)
-        self.entry_devolucion.config(width = 50, state = 'disabled', font = ('Arial', 11))
+        self.entry_devolucion.config(width = 50, font = ('Arial', 11))
         self.entry_devolucion.grid(row = 5, column = 1, padx = 8, pady = 8)
 
         self.entry_busqueda = tk.Entry(self)
-        self.entry_busqueda.config(width = 30, state = 'disabled', font = ('Arial', 11))
+        self.entry_busqueda.config(width = 30, font = ('Arial', 11))
         self.entry_busqueda.grid(row = 7, column = 0, padx = 10, pady = 10)
 
         self.entry_muestra = tk.Entry(self)
-        self.entry_muestra.config(width = 30, state = 'disabled', font = ('Arial', 11))
+        self.entry_muestra.config(width = 30, font = ('Arial', 11))
         self.entry_muestra.grid(columnspan = 2, row = 7, column = 1, padx = 10, pady = 10)
 
 
@@ -113,7 +153,7 @@ class Frame(tk.Frame):
         # Botones
         
         self.boton_registrar = tk.Button(self, text = 'Registrar')
-        self.boton_registrar.config(width = 20, font = ('Arial', 11, 'bold'), fg = 'lemon chiffon', bg = 'SteelBlue2',
+        self.boton_registrar.config(width = 20, font = ('Arial', 11, 'bold'), fg = 'snow', bg = 'SteelBlue2',
                                 cursor = 'hand2', activebackground = 'SkyBlue1')
         self.boton_registrar.grid(row = 6, column = 0, padx = 10, pady = 10)
 
@@ -128,14 +168,14 @@ class Frame(tk.Frame):
         self.boton_mostrar.grid(row = 6, column = 2, padx = 10, pady = 10)
 
         self.boton_buscar = tk.Button(self, text = 'Buscar por Curso')
-        self.boton_buscar.config(width = 15, font = ('Arial', 11, 'bold'), fg = 'lemon chiffon', bg = 'SteelBlue2',
-                                cursor = 'hand2', activebackground = 'SkyBlue1')
+        self.boton_buscar.config(width = 15, font = ('Arial', 11, 'bold'), fg = 'snow', bg = 'CadetBlue3',
+                                cursor = 'hand2', activebackground = 'CadetBlue1')
         self.boton_buscar.grid(row = 7, columnspan = 2, column = 0, pady = 10)
 
         
         self.boton_mostrarfecha = tk.Button(self, text = 'Buscar por Fecha')
-        self.boton_mostrarfecha.config(width = 15, font = ('Arial', 11, 'bold'), fg = 'lemon chiffon', bg = 'SteelBlue2',
-                                cursor = 'hand2', activebackground = 'SkyBlue1')
+        self.boton_mostrarfecha.config(width = 15, font = ('Arial', 11, 'bold'), fg = 'snow', bg = 'aquamarine3',
+                                cursor = 'hand2', activebackground = 'aquamarine')
         self.boton_mostrarfecha.grid(row = 7, column = 2)
 
     def tabla_datos(self):
